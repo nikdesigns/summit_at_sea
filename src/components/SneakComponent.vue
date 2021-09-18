9071856cc66097b5ad5375f0019bc7825e4db1c1
nikdesigns <nitinkaushikdesigns@gmail.com>
2021-09-18 15:28:57 +0530

    sneak peak tabs

---
+++ src/components/SneakComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Sneak</h2>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hero-component></hero-component>
+    <feature-component></feature-component>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import FeatureComponent from "../components/FeatureComponent.vue";
+import HeroComponent from "../components/HeroComponent.vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: { HeroComponent, FeatureComponent },
+  name: "Home",
+};
 </script>
